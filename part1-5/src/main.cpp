086acfc7001b8e5513b53ab815f7e44ca7d240bd
drunkyeast <jmjzhangqiao@gmail.com>
2025-01-21 21:11:51 +0800

    first commit

---
+++ part1-5/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/add.h"
+#include "../include/sub.h"
+int main() {
+
+    std::cout << add(1,3) << std::endl;
+    std::cout << sub(1,3) << std::endl;
+    return 0;
+}
